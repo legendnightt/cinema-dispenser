87109a5fe5764c601fac6dcf4838deaf2425b3e7
legendnightt <alexporrast@gmail.com>
2022-01-05 23:40:06 +0100

    created abstract Operation

--- src/cinemadispenser/Operation.java
+++ src/cinemadispenser/Operation.java
@@ -1,7 +1,39 @@
 package cinemadispenser;
 
-public class Operation {
+import sienens.CinemaTicketDispenser;
 
+public abstract class Operation {
 
+    private final CinemaTicketDispenser dispenser;
+    private final Multiplex multiplex;
+
+    public abstract void doOperation();
+    public abstract String getTitle();
+
+    /**
+     * Operation builder
+     * @param dispenser CinemaTicketDispenser dispenser
+     * @param multiplex Multiplex multiplex
+     */
+    public Operation(CinemaTicketDispenser dispenser, Multiplex multiplex) {
+        this.dispenser = dispenser;
+        this.multiplex = multiplex;
+    }
+
+    /**
+     * Gets CinemaTicketDispenser dispenser
+     * @return CinemaTicketDispenser dispenser
+     */
+    public CinemaTicketDispenser getDispenser() {
+        return dispenser;
+    }
+
+    /**
+     * Gets Multiplex multiplex
+     * @return Multiplex multiplex
+     */
+    public Multiplex getMultiplex() {
+        return multiplex;
+    }
 
 }
--- src/cinemadispenser/operations/MainMenu.java
+++ src/cinemadispenser/operations/MainMenu.java
@@ -1,9 +1,15 @@
 package cinemadispenser.operations;
 
+import cinemadispenser.Operation;
 import sienens.CinemaTicketDispenser;
 import urjc.UrjcBankServer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainMenu {
+
+    public List<Operation> operationList = new ArrayList<>();
 
     public MainMenu(CinemaTicketDispenser dispenser, UrjcBankServer bank) {
         IdiomSelection idiom = new IdiomSelection();
